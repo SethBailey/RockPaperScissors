8219a04fe0faa01b1fafb6ed31e926201f590ea8
Seth Bailey <Seth.joel.bailey@gmail.com>
2018-12-06 21:01:25 +0000

    first C# attempt

---
+++ Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace TextGame
+{
+    class Start
+    {
+        
+        const int Rock = 1;
+        const int Paper =2;
+        const int Scissors = 3;
+        static int playerscore = 0;
+        static int computerscore = 0;
+
+
+        //This is where your program starts
+        static void Main(string[] args)
+        {
+            PlayRockPaperScissors();
+        }
+
+        private static void PlayRockPaperScissors()
+        {
+            int computerPick = ComputerPickAWeapon();
+            int playerPick = PlayerPickAWeapon();
+            DisplayWinner(computerPick, playerPick);
+            Displayscorecard();
+            PlayAgain();
+        }
+
+        private static void Displayscorecard()
+        {
+            Console.WriteLine("player score: " + playerscore);
+            Console.WriteLine("computer score: " + computerscore);
+        }
+
+        private static void PlayAgain()
+        {
+            Console.Write("Continue? (y,n) ");
+            var playAgain = Console.ReadLine();
+            if (playAgain == "y")
+            {
+                PlayRockPaperScissors();
+            }
+            else
+            {
+                Console.WriteLine("Bye bye");
+            }
+            
+        }
+
+
+
+        private static void DisplayWinner(int computerPick, int playerPick)
+        {
+            DisplaysComputerPick(computerPick);
+            ShowResult(computerPick, playerPick);
+        }
+
+        private static void ShowResult(int computerPick, int playerPick)
+        {
+            //Draw
+            if (computerPick == playerPick)
+            {
+                Console.WriteLine(" draw ");
+                return;
+            }
+
+            //Player Win
+            if ( (playerPick == Paper && computerPick == Rock  ) ||
+                 (playerPick == Scissors  && computerPick == Paper ) ||
+                 (playerPick == Paper && computerPick == Scissors ) )
+            {
+                playerscore++;
+                Console.WriteLine(" Win! ");
+                return;
+            }
+            
+            //Player Lose
+            computerscore++;
+            Console.WriteLine(" Lose ");          
+        }
+
+        private static void DisplaysComputerPick(int computerPick)
+        {
+            Console.Write("Computer chose : ");
+            if (computerPick == Rock)
+            {
+                Console.WriteLine("rock");
+            }
+            else if (computerPick == Paper)
+            {
+                Console.WriteLine("paper");
+            }
+            else if (computerPick == Scissors)
+            {
+                Console.WriteLine("scissors");
+            }
+        }
+
+        private static int PlayerPickAWeapon()
+        {
+            Console.Write("Pick a weapon (r,p,s) : ");
+            var input = Console.ReadLine();
+            if (input == "r")
+            {
+                return Rock;
+            }
+            if (input == "p")
+            {
+                return Paper;
+            }
+            if (input == "s")
+            {
+               return Scissors; 
+            }
+
+            Console.WriteLine("You picked the wrong letter...try again");
+            return PlayerPickAWeapon();
+        }
+          
+       
+        private static int ComputerPickAWeapon()
+        {
+            var x = new Random();
+            return x.Next(1,3);
+        }
+
+
+    }
+
+}
